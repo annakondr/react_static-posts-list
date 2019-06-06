fbeccdb07fe2f181d31f0b1e7b72bf0c64506700
annakondr <tyukondra@gmail.com>
2019-06-06 19:49:52 +0300

    Updates

---
+++ precache-manifest.9d3e83aaea1a00359a273d7e85b03eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bad29e1c3f262bbfaf8f7b18ba67d4",
+    "url": "/react_static-posts-list/index.html"
+  },
+  {
+    "revision": "a67538808daed04f2ac4",
+    "url": "/react_static-posts-list/static/css/main.58a7c2cb.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-posts-list/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a67538808daed04f2ac4",
+    "url": "/react_static-posts-list/static/js/main.4dff85db.chunk.js"
+  },
+  {
+    "revision": "e2ce455a8d3cef8c01d5",
+    "url": "/react_static-posts-list/static/js/runtime~main.c1d34afe.js"
+  }
+]);
